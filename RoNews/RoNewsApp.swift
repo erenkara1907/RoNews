663ccec708b0b889588283677b6109b7c8e795e2
erenkara1907 <erenkaraaa47@gmail.com>
2022-12-20 16:32:52 +0300

    Initial Commit

---
+++ RoNews/RoNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoNewsApp.swift
+//  RoNews
+//
+//  Created by Eren Kara on 20.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct RoNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
